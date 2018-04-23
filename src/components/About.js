b866b5f96fec0fd7f741a7a868bffc63b3e17bf5
nada <nadin.vasic@gmail.com>
2018-04-23 12:53:49 +0200

    nista novo

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,9 @@ render() {
             <br/>
             <div className="container">
                 <h1>Online shop</h1>
-
                 <br/>
-
                 <p>
                 Buy digital version of the book or put your own content you want to share. 
-
-
                 </p>
             </div>
         </div>
